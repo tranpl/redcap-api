5f4191caa30efb8ab333a5fbf62b1106f1e97577
Michael Tran <tranpl@outlook.com>
2017-08-30 10:40:20 -0400

    update demo

--- RedcapApiDemo/Program.cs
+++ RedcapApiDemo/Program.cs
@@ -15,7 +15,7 @@ namespace RedcapApiDemo
             var redcap_api = new RedcapApi("3D57A7FA57C8A43F6C8803A84BB3957B", "http://localhost/redcap/api/");
 
             var armList = new List<int> {2 }.ToArray();
-            var result = redcap_api.DeleteArms(armList).Result;
+            var result = redcap_api.DeleteArmsAsync(armList).Result;
             var data = JsonConvert.DeserializeObject(result);
             Console.WriteLine(data);
             Console.ReadLine();
