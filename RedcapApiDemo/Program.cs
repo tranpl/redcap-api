c6698eabae50fb5ea7f5a01a67d2dba03ee80679
Michael Tran <tranpl@outlook.com>
2017-09-13 14:42:43 -0400

    .

--- RedcapApiDemo/Program.cs
+++ RedcapApiDemo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Newtonsoft.Json;
 using Redcap;
 using Redcap.Models;
@@ -15,7 +14,9 @@ namespace RedcapApiDemo
             var redcap_api = new RedcapApi("3D57A7FA57C8A43F6C8803A84BB3957B", "http://localhost/redcap/api/");
 
             var result = redcap_api.GetRecordAsync("1",RedcapFormat.json, RedcapDataType.flat, ReturnFormat.json, null, null, null, null).Result;
+
             var data = JsonConvert.DeserializeObject(result);
+
             Console.WriteLine(data);
             Console.ReadLine();
 
