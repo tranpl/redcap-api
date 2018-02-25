5464057a6f8bdef5fc3bd70d1f0caffb9976b83e
Michael Tran <tranpl@outlook.com>
2018-02-24 21:41:45 -0500

    move utility methods to extension

--- RedcapApi/Utilities/Utils.cs
+++ RedcapApi/Utilities/Utils.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Redcap.Models;
 using Serilog;
 
 namespace Redcap.Utilities
@@ -134,7 +136,331 @@ namespace Redcap.Utilities
                 return await Task.FromResult(String.Empty);
             }
         }
+        /// <summary>
+        ///The method hands the return content from a request, the response.
+        /// The method allows the calling method to choose a return type.
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="returnContent"></param>
+        /// <returns>string</returns>
+        public static async Task<string> HandleReturnContent(this RedcapApi redcapApi, ReturnContent returnContent = ReturnContent.count)
+        {
+            try
+            {
+                var _returnContent = returnContent.ToString();
+                switch (returnContent)
+                {
+                    case ReturnContent.ids:
+                        _returnContent = ReturnContent.ids.ToString();
+                        break;
+                    case ReturnContent.count:
+                        _returnContent = ReturnContent.count.ToString();
+                        break;
+                    default:
+                        _returnContent = ReturnContent.count.ToString();
+                        break;
+                }
+                return await Task.FromResult(_returnContent);
+            }
+            catch (Exception Ex)
+            {
+                Log.Error(Ex.Message);
+                return await Task.FromResult(String.Empty);
+            }
+        }
+        /// <summary>
+        /// Tuple that returns both inputFormat and redcap returnFormat
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="inputFormat">csv, json, xml [default], odm ('odm' refers to CDISC ODM XML format, specifically ODM version 1.3.1)</param>
+        /// <param name="returnFormat"></param>
+        /// <param name="redcapDataType"></param>
+        /// <returns>tuple, string, string, string</returns>
+        public static async Task<(string inputFormat, string returnFormat, string redcapDataType)> HandleFormat(this RedcapApi redcapApi, InputFormat? inputFormat = InputFormat.json, ReturnFormat? returnFormat = ReturnFormat.json, RedcapDataType? redcapDataType = RedcapDataType.flat)
+        {
+            // default
+            var _inputFormat = InputFormat.json.ToString();
+            var _returnFormat = ReturnFormat.json.ToString();
+            var _redcapDataType = RedcapDataType.flat.ToString();
 
+            try
+            {
+
+                switch (inputFormat)
+                {
+                    case InputFormat.json:
+                        _inputFormat = InputFormat.json.ToString();
+                        break;
+                    case InputFormat.csv:
+                        _inputFormat = InputFormat.csv.ToString();
+                        break;
+                    case InputFormat.xml:
+                        _inputFormat = InputFormat.xml.ToString();
+                        break;
+                    default:
+                        _inputFormat = InputFormat.json.ToString();
+                        break;
+                }
+
+                switch (returnFormat)
+                {
+                    case ReturnFormat.json:
+                        _returnFormat = ReturnFormat.json.ToString();
+                        break;
+                    case ReturnFormat.csv:
+                        _returnFormat = ReturnFormat.csv.ToString();
+                        break;
+                    case ReturnFormat.xml:
+                        _returnFormat = ReturnFormat.xml.ToString();
+                        break;
+                    default:
+                        _returnFormat = ReturnFormat.json.ToString();
+                        break;
+                }
+
+                switch (redcapDataType)
+                {
+                    case RedcapDataType.flat:
+                        _returnFormat = RedcapDataType.flat.ToString();
+                        break;
+                    case RedcapDataType.eav:
+                        _returnFormat = RedcapDataType.eav.ToString();
+                        break;
+                    case RedcapDataType.longitudinal:
+                        _returnFormat = RedcapDataType.longitudinal.ToString();
+                        break;
+                    case RedcapDataType.nonlongitudinal:
+                        _returnFormat = RedcapDataType.nonlongitudinal.ToString();
+                        break;
+                    default:
+                        _returnFormat = RedcapDataType.flat.ToString();
+                        break;
+                }
+
+                return await Task.FromResult((_inputFormat, _returnFormat, _redcapDataType));
+            }
+            catch (Exception Ex)
+            {
+                Log.Error(Ex.Message);
+                return await Task.FromResult((_inputFormat, _returnFormat, _redcapDataType));
+            }
+        }
+
+        /// <summary>
+        /// Method gets the overwrite behavior type and converts into string
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="overwriteBehavior"></param>
+        /// <returns>string</returns>
+        public static async Task<string> ExtractBehaviorAsync(this RedcapApi redcapApi, OverwriteBehavior overwriteBehavior)
+        {
+            try
+            {
+                var _overwriteBehavior = OverwriteBehavior.normal.ToString();
+                switch (overwriteBehavior)
+                {
+                    case OverwriteBehavior.overwrite:
+                        _overwriteBehavior = OverwriteBehavior.overwrite.ToString();
+                        break;
+                    case OverwriteBehavior.normal:
+                        _overwriteBehavior = OverwriteBehavior.normal.ToString();
+                        break;
+                    default:
+                        _overwriteBehavior = OverwriteBehavior.overwrite.ToString();
+                        break;
+                }
+                return await Task.FromResult(_overwriteBehavior);
+            }
+            catch (Exception Ex)
+            {
+                Log.Error($"{Ex.Message}");
+                return await Task.FromResult(String.Empty);
+            }
+        }
+        /// <summary>
+        /// This method extracts and converts an object's properties and associated values to redcap type and values.
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="input">Object</param>
+        /// <returns>Dictionary of key value pair.</returns>
+        public static async Task<Dictionary<string, string>> GetProperties(this RedcapApi redcapApi, object input)
+        {
+            try
+            {
+                if (input != null)
+                {
+                    // Get the type
+                    var type = input.GetType();
+                    var obj = new Dictionary<string, string>();
+                    // Get the properties
+                    var props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                    PropertyInfo[] properties = input.GetType().GetProperties();
+                    foreach (var prop in properties)
+                    {
+                        // get type of column
+                        // The the type of the property
+                        Type columnType = prop.PropertyType;
+
+                        // We need to set lower case for REDCap's variable nameing convention (lower casing)
+                        string propName = prop.Name.ToLower();
+                        // We check for null values
+                        var propValue = type.GetProperty(prop.Name).GetValue(input, null)?.ToString();
+                        if (propValue != null)
+                        {
+                            var t = columnType.GetGenericArguments();
+                            if (t.Length > 0)
+                            {
+                                if (columnType.GenericTypeArguments[0].FullName == "System.DateTime")
+                                {
+                                    var dt = DateTime.Parse(propValue);
+                                    propValue = dt.ToString();
+                                }
+                                if (columnType.GenericTypeArguments[0].FullName == "System.Boolean")
+                                {
+                                    if (propValue == "True")
+                                    {
+                                        propValue = "1";
+                                    }
+                                    else
+                                    {
+                                        propValue = "0";
+                                    }
+                                }
+                            }
+                            obj.Add(propName, propValue);
+                        }
+                        else
+                        {
+                            // We have to make sure we handle for null values.
+                            obj.Add(propName, null);
+                        }
+                    }
+                    return await Task.FromResult(obj);
+                }
+                return await Task.FromResult(new Dictionary<string, string> { });
+            }
+            catch (Exception Ex)
+            {
+                Log.Error($"{Ex.Message}");
+                return await Task.FromResult(new Dictionary<string, string> { });
+            }
+        }
+        /// <summary>
+        /// Method extracts events into list from string
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="events"></param>
+        /// <param name="delimiters">char[] e.g [';',',']</param>
+        /// <returns>List of string</returns>
+        public static async Task<List<string>> ExtractEventsAsync(this RedcapApi redcapApi, string events, char[] delimiters)
+        {
+            if (!String.IsNullOrEmpty(events))
+            {
+                try
+                {
+                    var formItems = events.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                    List<string> eventsResult = new List<string>();
+                    foreach (var form in formItems)
+                    {
+                        eventsResult.Add(form);
+                    }
+                    return await Task.FromResult(eventsResult);
+                }
+                catch (Exception Ex)
+                {
+                    Log.Error($"{Ex.Message}");
+                    return await Task.FromResult(new List<string> { });
+                }
+            }
+            return await Task.FromResult(new List<string> { });
+        }
+        /// <summary>
+        /// Method gets / extracts fields into list from string
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="fields"></param>
+        /// <param name="delimiters">char[] e.g [';',',']</param>
+        /// <returns>List of string</returns>
+        public static async Task<List<string>> ExtractFieldsAsync(this RedcapApi redcapApi, string fields, char[] delimiters)
+        {
+            if (!String.IsNullOrEmpty(fields))
+            {
+                try
+                {
+                    var fieldItems = fields.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                    List<string> fieldsResult = new List<string>();
+                    foreach (var field in fieldItems)
+                    {
+                        fieldsResult.Add(field);
+                    }
+                    return await Task.FromResult(fieldsResult);
+                }
+                catch (Exception Ex)
+                {
+                    Log.Error($"{Ex.Message}");
+                    return await Task.FromResult(new List<string> { });
+                }
+            }
+            return await Task.FromResult(new List<string> { });
+        }
+        /// <summary>
+        /// Method gets / extract records into list from string
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="records"></param>
+        /// <param name="delimiters">char[] e.g [';',',']</param>
+        /// <returns>List of string</returns>
+        public static async Task<List<string>> ExtractRecordsAsync(this RedcapApi redcapApi, string records, char[] delimiters)
+        {
+            if (!String.IsNullOrEmpty(records))
+            {
+                try
+                {
+                    var recordItems = records.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                    List<string> recordResults = new List<string>();
+                    foreach (var record in recordItems)
+                    {
+                        recordResults.Add(record);
+                    }
+                    return await Task.FromResult(recordResults);
+                }
+                catch (Exception Ex)
+                {
+                    Log.Error($"{Ex.Message}");
+                    return await Task.FromResult(new List<string> { });
+                }
+            }
+            return await Task.FromResult(new List<string> { });
+        }
+        /// <summary>
+        /// Method gets / extracts forms into list from string
+        /// </summary>
+        /// <param name="redcapApi"></param>
+        /// <param name="forms"></param>
+        /// <param name="delimiters">char[] e.g [';',',']</param>
+        /// <returns>A list of string</returns>
+        public static async Task<List<string>> ExtractFormsAsync(this RedcapApi redcapApi, string forms, char[] delimiters)
+        {
+            if (!String.IsNullOrEmpty(forms))
+            {
+                try
+                {
+                    var formItems = forms.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                    List<string> formsResult = new List<string>();
+                    foreach (var form in formItems)
+                    {
+                        formsResult.Add(form);
+                    }
+                    return await Task.FromResult(formsResult);
+                }
+                catch (Exception Ex)
+                {
+                    Log.Error($"{Ex.Message}");
+                    return await Task.FromResult(new List<string> { });
+                }
+            }
+            return await Task.FromResult(new List<string> { });
+        }
 
     }
 }
