284493b032dbe4f9322d5a9c0e66bb50ead05357
Michael Tran <tranpl@outlook.com>
2019-02-13 14:14:03 -0500

    add instrument model

---
+++ RedcapApi/Models/RedcapInstrument.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Redcap.Models
+{
+    /// <summary>
+    /// Instrument in a redcap project
+    /// </summary>
+    public class RedcapInstrument
+    {
+        /// <summary>
+        /// Name of the instrument
+        /// </summary>
+        [JsonProperty("instrument_name")]
+        public string InstrumentName { get; set; }
+
+        /// <summary>
+        /// Label (display)
+        /// </summary>
+        [JsonProperty("instrument_label")]
+        public string InstrumentLabel { get; set; }
+
+    }
+}
