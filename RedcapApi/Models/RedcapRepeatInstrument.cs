bf486abf9ce7a1f576e97ccf61a8d84233949f83
Michael Tran <tranpl@outlook.com>
2019-02-23 16:54:36 -0500

    update models to support updated API
    add new model
    adjusted existing models

---
+++ RedcapApi/Models/RedcapRepeatInstrument.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Redcap.Models
+{
+    /// <summary>
+    /// Redcap Repeat Instrument
+    /// The instrument that is repeated in a project that has repeating instruments enabled.
+    /// This can support "repeat entire event" or "repeat instruments" mode.
+    /// </summary>
+    public class RedcapRepeatInstrument: RedcapEvent
+    {
+
+        /// <summary>
+        /// The unique instrument/form name that is repeated for the specific event.
+        /// e.g demographics
+        /// </summary>
+        [JsonProperty("form_name")]
+        public string FormName { get; set; }
+
+        /// <summary>
+        /// The custom form/instrument label for this repeating instrument or event.
+        /// </summary>
+        [JsonProperty("custom_form_label")]
+        public string CustomFormLabel { get; set; }
+    }
+}
