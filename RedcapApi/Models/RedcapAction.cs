3340f6a3bf2c515290bc6d100cbc3a63a8652e0e
Michael Tran <tranpl@outlook.com>
2024-10-17 18:47:09 -0400

    add new action for randomization 2.0 support

--- RedcapApi/Models/RedcapAction.cs
+++ RedcapApi/Models/RedcapAction.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Redcap.Models
 {
@@ -51,6 +47,12 @@ namespace Redcap.Models
         /// Rename
         /// </summary>
         [Display(Name = "rename")]
-        Rename
+        Rename,
+
+        /// <summary>
+        /// Randomize
+        /// </summary>
+        [Display(Name = "randomize")]
+        Randomize
     }
 }
